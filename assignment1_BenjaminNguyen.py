884d73f471beee732eb30e8c78cbfcdf4704852d
Benjamin <bennguyenhb@gmail.com>
2024-02-07 15:39:38 -0800

    idk

--- assignment1_BenjaminNguyen.py
+++ assignment1_BenjaminNguyen.py
@@ -3,21 +3,21 @@
 # Completed (or last revision): 02/08/2024
 """
 # Task 1: Practice arithmetic expressions
-PV = float(input("Enter the present dollar amount (float): "))
+PV = float(input("Enter the present dollar amount (float): $"))
 INT = float(input("Enter the interest rate (float): "))
 YRS = int(input("Enter the number of years (integer): "))
 print("The future value is $%.2f" % (PV * (1 + INT / 100) ** YRS))
 
 # tests' outputs
-Enter the present dollar amount (float): 1000.0
+Enter the present dollar amount (float): $1000.0
 Enter the interest rate (float): 5.0
 Enter the number of years (integer): 30
 The future value is $4321.94
-Enter the present dollar amount (float): 1530.50
+Enter the present dollar amount (float): $1530.50
 Enter the interest rate (float): 3.5
 Enter the number of years (integer): 15
 The future value is $2564.12
-Enter the present dollar amount (float): 100.0
+Enter the present dollar amount (float): $100.0
 Enter the interest rate (float): 1.0
 Enter the number of years (integer): 10
 The future value is $110.46
@@ -40,3 +40,5 @@ Enter a number (integer): 1
 Before the swap, the first number was 10 ,and the second number was 1
 After the swap, the first number is 1 ,and the second number is 10
 """
+num1 = int(input("Enter the value of num1 and num2 "))
+num2 = int(input())
