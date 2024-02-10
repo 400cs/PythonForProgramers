9e0ef6420d1688b2c796cf8c99000a013d454457
Benjamin <bennguyenhb@gmail.com>
2024-02-09 20:03:57 -0800

    blah

--- Z4_BenjaminNguyen.py
+++ Z4_BenjaminNguyen.py
@@ -3,3 +3,7 @@
 # Completed (or last revision): 02/10/2024
 
 # Problem 1
+a = int(input("Enter in a number for a: "))
+b = int(input("Enter in a number for b: "))
+n = int(input("Enter in a number for n: "))
+for i in range(a, b + 1):
